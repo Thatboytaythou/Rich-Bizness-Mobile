275ef28af0e2d90c5715ff8c811663f17437fdba
Thatboytaythou <tayben.h@gmail.com>
2026-05-09 19:19:10 -0400

    Update engine.js

--- core/visual/engine.js
+++ core/visual/engine.js
@@ -1,1 +1,23 @@
+export function startEngine(){
 
+  console.log("ENGINE STARTED");
+
+  const app = document.getElementById("app");
+
+  if(!app){
+    console.error("NO #app FOUND");
+    return;
+  }
+
+  const text = document.createElement("div");
+
+  text.innerText = "RICH BIZNESS LOADING...";
+  text.style.color = "#00ffcc";
+  text.style.fontSize = "24px";
+  text.style.position = "absolute";
+  text.style.top = "50%";
+  text.style.left = "50%";
+  text.style.transform = "translate(-50%, -50%)";
+
+  app.appendChild(text);
+}
